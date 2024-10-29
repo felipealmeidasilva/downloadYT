7b3633bc4bcf418c9c9cabd4545c42de6e4317cc
Felipe Almeida Silva <filipealmeidakukri@hotmail.com>
2024-10-29 09:13:03 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import ssl
+from pytube import YouTube
+from pytube import request
+from pytube import extract
+from pytube.innertube import _default_clients
+from pytube.exceptions import RegexMatchError
+
+_default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
+_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID"]
+
+import pytube, re
+def patched_get_throttling_function_name(js: str) -> str:
+    function_patterns = [
+        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&.*?\|\|\s*([a-z]+)',
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)',
+        r'\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)',
+    ]
+    for pattern in function_patterns:
+        regex = re.compile(pattern)
+        function_match = regex.search(js)
+        if function_match:
+            if len(function_match.groups()) == 1:
+                return function_match.group(1)
+            idx = function_match.group(2)
+            if idx:
+                idx = idx.strip("[]")
+                array = re.search(
+                    r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
+                        nfunc=re.escape(function_match.group(1))),
+                    js
+                )
+                if array:
+                    array = array.group(1).strip("[]").split(",")
+                    array = [x.strip() for x in array]
+                    return array[int(idx)]
+
+    raise RegexMatchError(
+        caller="get_throttling_function_name", pattern="multiple"
+    )
+
+ssl._create_default_https_context = ssl._create_unverified_context
+pytube.cipher.get_throttling_function_name = patched_get_throttling_function_name
+
+entrada = int(input("Digite 1 para Apenas AUDIO e 2 para VIDEO: "))
+link = input("Digite o link do Youtube: ")
+match entrada:
+    case 1:
+        yt = YouTube(link)
+        audio_stream = yt.streams.filter(only_audio=True).first()    
+        audio_filename = audio_stream.download(output_path = "./output")
+    case 2:
+        yt = YouTube(link)
+        video_stream = yt.streams.first()
+        video_filename = video_stream.download(output_path = "./output")
+    case _:
+        print("Opção errada")
